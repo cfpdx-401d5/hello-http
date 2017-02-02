c27633680c6fe1fce655e4f44bf899f052d31f21
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-02 12:24:58 -0800

    passing tests for greeting, name and salutation

--- test/http-server.test.js
+++ test/http-server.test.js
@@ -7,11 +7,29 @@ chai.use(chaiHttp);
 describe('testing servers with chai-http', () => {
     const request = chai.request(server);
 
-    it('GET / says I guess', done => {
+    it('GET / hello stranger', done => {
         request
             .get('/')
             .end((err, res) => {
-                assert.strictEqual(res.text, 'get');
+                assert.strictEqual(res.text, 'hello stranger');
+                done();
+            });
+    });
+
+    it('GET / hello <name>', done => {
+        request
+            .get('/greeting/brigitte')
+            .end((err, res) => {
+                assert.strictEqual(res.text, 'hello brigitte');
+                done();
+            });
+    });
+
+    it('GET / salutation <name>', done => {
+        request
+            .get('/greeting/brigitte?salutation=hola')
+            .end((err, res) => {
+                assert.strictEqual(res.text, 'hola brigitte');
                 done();
             });
     });
