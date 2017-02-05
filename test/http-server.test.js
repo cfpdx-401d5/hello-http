646a35c9e23f5d25952383f6a6a2381c2fb933b9
mizutombo <thomas.shultz52@gmail.com>
2017-02-04 17:48:21 -0800

    repairs http-server.js code and validates functionality of http-server.test.js test code

--- test/http-server.test.js
+++ test/http-server.test.js
@@ -2,60 +2,90 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
-const server = require('../httpCowSayFiglet');
+const server = require('../lib/http-server');
  
-describe('test my httpCowSayFiglet server', () => {
+describe('test the http server :', () => {
 
-  const request = chai.request(server);
+	const request = chai.request(server);
 
-  it('test "happy_cow" url', done => {
-    request
-      .get('/happy_cow')
-      .end((err, res) => {
-        if (err) return done(err);
-        assert.strictEqual(res.text, 'hello wonderful world!');
-        done();
-      });
-  });
+	// it('test "happy_cow" url', done => {
+	// 	request
+  //     .get('/happy_cow')
+  //     .end((err, res) => {
+	// 			if (err) return done(err);
+	// 			assert.strictEqual(res.text, 'hello wonderful world!');
+	// 			done();
+	// 		});
+	// });
 
-  it('test "angry_cow" url', done => {
-    request
-      .get('/angry_cow')
-      .end((err, res) => {
-        if (err) return done(err);
-        assert.strictEqual(res.text, 'eat cow pies and die!!!');
-        done();
-      });
-  });
+	it('tests "/greeting" url', done => {
+		request
+			.get('/greeting')
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.strictEqual(res.text, 'hello stranger');
+				done();
+			});
+	});
 
-  it('test "halloween" url', done => {
-    request
-      .get('/halloween')
-      .end((err, res) => {
-        if (err) return done(err);
-        assert.strictEqual(res.text, 'happy halloween!');
-        done();
-      });
-  });
+	it('tests "/greeting/<name>" url', done => {
+		request
+			.get('/greeting/Zen')
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.strictEqual(res.text, 'hello Zen');
+				done();
+			});
+	});
 
-  it('test "spider" url', done => {
-    request
-      .get('/spider')
-      .end((err, res) => {
-        if (err) return done(err);
-        assert.strictEqual(res.text, '"Spiders from Angry Red Planet Mars"');
-        done();
-      });
-  });
+	it('tests "/salutation//<altGreeting>" url', done => {
+		request
+			.get('/salutation//willkommen')
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.strictEqual(res.text, 'willkommen stranger');
+				done();
+			});
+	});	
 
-  it('test "/" url', done => {
-    request
-      .get('/')
-      .end((err, res) => {
-        if (err) return done(err);
-        assert.strictEqual(res.text, '404 - Not Found ==> try URLs ... /halloween or /happy_cow or /angry_cow');
-        done();
-      });
-  });
+	it('tests "/salutation/<name>/<altGreeting>" url', done => {
+		request
+			.get('/salutation/Zen/willkommen')
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.strictEqual(res.text, 'willkommen Zen');
+				done();
+			});
+	});
+
+	it('tests "/fact" url', done => {
+		request
+			.get('/fact')
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.strictEqual(res.text, 'HTTP is short for hyper-text transfer protocol');
+				done();
+			});
+	});
+
+	it('tests "/" url', done => {
+		request
+			.get('/')
+			.end((err, res) => { 
+				if (err) return done(err);		
+				assert.deepEqual(res.text, '404 - Not Found ... try adding "/greeting" to this url');
+				done();
+			});
+	});
+
+	it('tests url method other than "/GET" url', done => {
+		request
+			.post('/salutation/Zen/hola')
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.strictEqual(res.text, '404 - http method POST is not supported');
+				done();
+			});
+	});
 
 });
