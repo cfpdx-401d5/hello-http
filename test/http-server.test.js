b720d55aa9e8cbb488de1b8feea20a534d124ac8
cmd-kvn <kvn.m.wong@gmail.com>
2017-02-07 10:20:11 -0800

    finished test for post allowing successful requests

--- test/http-server.test.js
+++ test/http-server.test.js
@@ -9,90 +9,114 @@ chai.use(chaiHttp);
 
 const server = require('../lib/http-server');
 
-describe('test the http server', () => {
+describe('test the GET request', () => {
 
     const request = chai.request(server);
 
-    it('responds with a 200 status code for a successful request', done => {
+    it('responds with a 200 status code for a successful GET request', done => {
         request
-        .get('/')
-        .end((err, res) => {
-            expect(res.statusCode).to.equal(200);
-            done();
-        });
+            .get('/')
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
     });
 
     it('responds with a 404 status code for an unsuccessful request', done => {
         request
-        .post('/')
-        .end((err, res) => {
-            expect(res.statusCode).to.equal(404);
-            done();
-        });
+            .delete('/')
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(404);
+                done();
+            });
     });
 
     it('GET/ responds with instruction to add "greeting" or "fact" in the path', done => {
         request
-        .get('/')
-        .end((err, res) => {
-            expect(res.text).to.be.at.least('Try adding "greeting');
-            done();
-        });
+            .get('/')
+            .end((err, res) => {
+                expect(res.text).to.be.at.least('Try adding "greeting');
+                done();
+            });
     });
 
     it('GET/fact responds with a fact', done => {
         request
-        .get('/fact')
-        .end((err, res) => {
-            expect(res.text).to.be.at.least('from Wikipedia:\nThe first version');
-            done();
-        });
+            .get('/fact')
+            .end((err, res) => {
+                expect(res.text).to.be.at.least('from Wikipedia:\nThe first version');
+                done();
+            });
     });
 
     it('GET/greeting responds with "Hello stranger!"', done => {
         request
-        .get('/greeting')
-        .end((err, res) => {
-            assert.equal(res.text, 'Hello stranger!');
-            done();
-        })
+            .get('/greeting')
+            .end((err, res) => {
+                assert.equal(res.text, 'Hello stranger!');
+                done();
+            })
     });
 
     it('GET/greeting/<name> responds with "Hello <name>!"', done => {
         request
-        .get('/greeting/kevin')
-        .end((err, res) => {
-            assert.equal(res.text, 'Hello Kevin!');
-            done();
-        });
+            .get('/greeting/kevin')
+            .end((err, res) => {
+                assert.equal(res.text, 'Hello Kevin!');
+                done();
+            });
     });
 
     it('GET with a <= 3 part path responds appropriately', done => {
         request
-        .get('/1/2/3')
-        .end((err, res) => {
-            expect(res.text).to.be.at.least('You have gone down a path');
-            done();
-        });
+            .get('/1/2/3')
+            .end((err, res) => {
+                expect(res.text).to.be.at.least('You have gone down a path');
+                done();
+            });
     });
 
     it('GET responds with the given salutation in the query string for a stranger', done => {
         request
-        .get('/greeting?pass=no_fail&salutation=howdy')
-        .end((err, res) => {
-            assert.equal(res.text, 'howdy stranger!');
-            done();
-        });
+            .get('/greeting?pass=no_fail&salutation=howdy')
+            .end((err, res) => {
+                assert.equal(res.text, 'howdy stranger!');
+                done();
+            });
     });
 
     it('GET responds with the given salutation in the query string for <name>', done => {
         request
-        .get('/greeting/kevin')
-        .query({salutation: 'hola'})
-        .end((err, res) => {
-            assert.equal(res.text, 'hola Kevin!');
-            done();
-        });
+            .get('/greeting/kevin')
+            .query({ salutation: 'hola' })
+            .end((err, res) => {
+                assert.equal(res.text, 'hola Kevin!');
+                done();
+            });
     });
+
+});
+
+describe('test the POST request', () => {
+
+    const request = chai.request(server);
+
+    it('responds with a 200 status code for a successful POST request', done => {
+        request
+            .post('/')
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
+    });
+
+    it.skip('POST only posts to /facts', () => {
+        request
+            .post('/facts')
+            .end((err, res) => {
+
+                done(); 
+            });
+    })
 
 });
