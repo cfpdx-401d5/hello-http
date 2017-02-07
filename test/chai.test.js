4bf09e67804c24fa8d7677f9b14ac9eeb0b56f66
mericks <mericks@users.noreply.github.com>
2017-02-06 21:08:58 -0800

    installed and tested superagent

--- test/chai.test.js
+++ test/chai.test.js
@@ -1,15 +1,25 @@
 const chai = require('chai');
 const assert = chai.assert;
+const request = require('superagent');
 
 describe('chai assertion library', () => {
-
-    it('has cool convenience methods - fail', () => {
-        assert.isAbove(2, 5);
-    });
 
     it('has cool convenience methods - pass', () => {
         assert.isAbove(5, 2);
     });
-    
+
+})
+
+describe('superagent', () => {
+
+    it('gets google', done => {
+        request
+            .get('http://google.com')
+            .end((err, res) => {
+                assert.isOk(res.text);
+                console.log(res.text);
+                done();
+            })
+    });
 
 })
